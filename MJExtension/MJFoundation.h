88c8bc933fd1f4a4d4e59f1771381a6a43627d9d
Hello_kugou <18201145070@163.com>
2016-06-13 16:09:59 +0800

    哈哈哈

--- MJExtension/MJFoundation.h
+++ MJExtension/MJFoundation.h
@@ -4,8 +4,8 @@
 //
 //  Created by MJ Lee on 14/7/16.
 //  Copyright (c) 2014年 小码哥. All rights reserved.
-//
-
+// Copyright (c) 2014年 小码哥. All rights reserved.
+// 哈哈哈哈哈哈哈哈啊哈哈啊哈哈哈哈哈哈哈哈哈哈哈哈哈
 #import <Foundation/Foundation.h>
 
 @interface MJFoundation : NSObject
